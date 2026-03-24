4afd533c5a1f1877002c8d6e319419dd5ca31369
Aayushgaut1 <aayushgautam801@gmail.com>
2026-03-24 15:10:08 +0530

    UC5:Booking request

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,27 +1,38 @@
-public class BookMyStayApp{
+public class BookMyStayApp {
+
     public static void main(String[] args) {
+
         /* ---------------- UC1 ---------------- */
         System.out.println("==============================================");
         System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("Book My Stay App - Version 2.1");
+        System.out.println("Book My Stay App - Version 3.1");
         System.out.println("==============================================");
         System.out.println("System initialized successfully.\n");
-        /* ---------------- UC2 ---------------- */
+
+        /* ---------------- UC2 + UC3 ---------------- */
         System.out.println("Hotel Room Initialization\n");
+
+        // Room objects (Domain)
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+
+        // Centralized inventory (State)
+        RoomInventory inventory = new RoomInventory();
+
         System.out.println("Single Room:");
         singleRoom.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable);
+        System.out.println("Available: " +
+                inventory.getRoomAvailability().get("Single"));
+
         System.out.println("\nDouble Room:");
         doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable);
+        System.out.println("Available: " +
+                inventory.getRoomAvailability().get("Double"));
+
         System.out.println("\nSuite Room:");
         suiteRoom.displayRoomDetails();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("Available: " +
+                inventory.getRoomAvailability().get("Suite"));
     }
 }
